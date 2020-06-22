3e70b65c568de5005ed922d20831771e2ce2f03f
RafaelNeves <53528153+RafaelNevesdev@users.noreply.github.com>
2020-06-22 01:02:58 -0300

    Deleted comments in scripts

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -16,8 +16,6 @@ function getCities(event){
     const citySelect = document.querySelector("[name=city]")
     const stateInput = document.querySelector("[name=state]")
 
-    //toda ver que for chamado ufValue, sumir a opção state na url
-
     const ufValue = event.target.value
 
     const indexOfSelectedState = event.target.selectedIndex
@@ -25,7 +23,6 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-    //limpar campo de cidade e não acumular com pesquisas anteriores
     citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
     citySelect.disabled = true
 
@@ -39,46 +36,38 @@ function getCities(event){
         for (const city of cities){
             citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
-        //deixar falso disable para campo de cidade após ter selecionado estado
         citySelect.disabled = false
     })
 }
 
-document //escuta se um evento na tag select "UF" foi alterado)
+document 
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
 
-// itens de coleta
+
 
 const itemsToCollect = document.querySelectorAll(".items-grid li")
 for (const item of itemsToCollect){
     item.addEventListener("click", handleSelectedItem)
 }
 
-//**** atualizar o campo escondido com os dados selecionados
 const collectedItems = document.querySelector("input[name=items")
-//adiciona valores no array, e esses valores são alterados conforme a seleção da box
+
 let selectedItems = []
 
 function handleSelectedItem(event){
     const itemLi = event.target
     
-    //adicionar ou remover uma classe com javascript
-    // toggle = adiciona ou remove uma classe
     itemLi.classList.toggle("selected")
 
     const itemId = itemLi.dataset.id
 
-    //verificar se existem itens selecionados, se sim
-    // pegar os itens selecionados
     const alreadySelected = selectedItems.findIndex( function(item){
-        const itemFound = item == itemId // true or false
+        const itemFound = item == itemId
         return itemFound
     })
 
-    // Se já estiver selecionado, tirar da seleção
     if( alreadySelected >= 0){
-        //tirar da seleção
         const filteredItems =selectedItems.filter(function(item){
             const itemIsDifferent = item != itemId
             return itemIsDifferent
@@ -86,12 +75,10 @@ function handleSelectedItem(event){
 
         selectedItems = filteredItems
         
-    // Se não estiver selecionado, adicionar à seleção
     }else{
         selectedItems.push(itemId)
     }
 
-    //**** atualizar o campo escondido com os dados selecionados
     collectedItems.value = selectedItems
 
 }
